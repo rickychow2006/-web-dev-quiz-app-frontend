57097608a0cca4e67b5b62a8b33a5dbab7d56368
rickychow2006 <rickychow0716@gmail.com>
2021-12-09 22:31:30 -0600

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
-const heading = document.getElementById('heading');
+const header = document.getElementById('header');
+const userForm = document.querySelector('.input-group');
 const loginBtn = document.getElementById('login-btn');
-const playBtn = document.getElementById('play-btn');
-const containerDiv = document.querySelector('.app-container');
+const userNote = document.querySelector('.user-note');
+const playBtn = document.getElementById('play');
+const containerDiv = document.querySelector('.container');
 
 let currentCategory = '';
 
 playBtn.addEventListener('click', () => {
     containerDiv.innerHTML = '';
     const infoDiv = document.createElement('div')
-        infoDiv.className = ''
-        infoDiv.innerHTML = `
-        <div class="header">
-        <span> Some Rules of this Quiz </span> </div>
-        </div>
+        infoDiv.className = 'position-relative'
+        infoDiv.innerHTML = `<div class =" text-center m-3 border-bottom"">
+        <h3> Some Rules of this Quiz </h3> </div>
         <div class="info-list">
-        <div class="info"> 1. Once you select your answer, it can't be undone. </div>
-        <div class="info"> 2. You can't select any option once time goes off. </div>
-        <div class="info"> 3. You can't exit from the Quiz while you're playing. </div>
-        <div class="info"> 4. You'll get points on the basis of your correct answers.</div> 
-        </div>
-        <div class="buttons">
-            <button class="exit_btn "> Exit Quiz </button>
-            <button class="continue_btn"> Continue </button>
+        <div class="info m-3"> 1. Once you select your answer, it can't be undone. </div>
+        <div class="info m-3"> 2. You can't exit from the Quiz while you're playing. </div>
+        <div class="info m-3 border-bottom "> 3. You'll get points on the basis of your correct answers. <br> <br> </div> </div>
+        <div class="btn float-end">
+            <button class="exit_btn btn btn-outline-secondary"> Exit </button>
+            <button class="continue_btn btn btn-outline-secondary"> Continue </button>
         </div>`
         containerDiv.appendChild(infoDiv);
 
@@ -29,6 +27,16 @@ playBtn.addEventListener('click', () => {
         const continueBtn = document.querySelector('.continue_btn')
 
         exitBtn.addEventListener('click', () => {
-            window.location.reload()
+        window.location.reload()
+        })
+
+        continueBtn.addEventListener('click', () => {
+            containerDiv.innerHTML = `
+            <div class =" text-center m-3 border-bottom">
+            <h3>Select Category</h3> </div>`;
+            getCategories = () => fetch(`http://localhost:3000/api/v1/categories`).then(resp => resp.json())
+            getCategories()
+                .then(categories => categories.data.forEach(category => new Category(category.attributes.name)))
+                .catch(error => alert(error));
         })
 })
